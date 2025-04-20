f54f9bf5a33b59100b642393979012e7af053af1
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-20 16:59:13 +0530

    init

---
+++ 02/fifth.java
@@ -0,0 +1,18 @@
+public class fifth {
+    public static void main(String args[]) {
+
+        int a = 12;
+        int b = 6;
+        String firstName = "Pogo";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a + b);
+        System.err.println(firstName);
+
+        b = 90; // changing the value of the variable
+
+        System.out.println(b);
+
+    }
+}
