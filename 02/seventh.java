e249038fc7ff087e8117e70c812a3c520a375c1a
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 18:06:51 +0530

    init

--- 02/seventh.java
+++ 02/seventh.java
@@ -3,6 +3,7 @@ public class seventh {
     public static void main(String args[]) {
         int a = 23;
         int b = 9;
-        System.out.println(a + b);
+        int sum = a + b;
+        System.out.println(sum);
     }
 }
