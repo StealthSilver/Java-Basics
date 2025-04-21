4c5b64611dd0eb9cd1c5404a6d707dc0f763420b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 18:12:12 +0530

    init

--- 02/eighth.java
+++ 02/eighth.java
@@ -1,9 +1,11 @@
 // taking the input from the user
 
+// importing the package which contains the scanner class
 import java.util.*;
 
 public class eighth {
     public static void main(String args[]) {
+        // creating the new object from the scanner class
         Scanner sc = new Scanner(System.in);
     }
 }
