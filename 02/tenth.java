520714bd3684d00a13b7b0797bbbb27293f6f443
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 18:21:27 +0530

    init

---
+++ 02/tenth.java
@@ -0,0 +1,14 @@
+// getting numbers from the user and summing them
+
+import java.util.*;
+
+public class tenth {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+}
