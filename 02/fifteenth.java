2864f6284ebab85a0de80f2b6698d4f25b97410e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 18:41:12 +0530

    init

---
+++ 02/fifteenth.java
@@ -0,0 +1,15 @@
+// explicit converting the char data type 
+
+public class fifteenth {
+    public static void main(String args[]) {
+        char ch = 'a';
+        char ch2 = 'b';
+
+        int num1 = ch;
+        int num2 = ch2;
+
+        // the ASCII values of the characters will be given
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
