48c488e1a45858af540473092f7e2b3a599ba7e1
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 18:24:55 +0530

    init

---
+++ 02/eleventh.java
@@ -0,0 +1,16 @@
+// getting two numebrs from the user and getting their product
+
+import java.util.*;
+
+public class eleventh {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+
+        float product = a * b;
+        System.out.println(product);
+
+    }
+}
