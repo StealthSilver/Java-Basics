3849167fbfff0d591f916cfe78fb661ca26fe517
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 20:02:04 +0530

    init

---
+++ 03/first.java
@@ -0,0 +1,10 @@
+// arthematic operators 
+
+public class first {
+    public static void main(String args[]) {
+        int a = 32;
+        int b = 9;
+
+        System.out.println("Sum is " + (a + b));
+    }
+}
