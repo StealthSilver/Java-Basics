697b85d119f289ccc623fc558e506cef41b9446e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 18:35:01 +0530

    init

---
+++ 02/thirteenth.java
@@ -0,0 +1,14 @@
+// implicit conversion
+
+import java.util.*;
+
+public class thirteenth {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        float number = sc.nextInt();
+        System.out.println(number);
+
+    }
+}
