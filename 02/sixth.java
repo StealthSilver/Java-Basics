2b2d58950f6c25d27aee6263e6f18c1d3ca2dc29
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 17:54:03 +0530

    init

---
+++ 02/sixth.java
@@ -0,0 +1,20 @@
+
+public class sixth {
+    public static void main(String args[]) {
+        byte b = 8;
+        System.out.println(b);
+
+        char ch = 'a';
+        System.out.println(ch);
+
+        boolean var = true;
+        System.out.println(var);
+
+        float price = (float) 10.4;
+        System.out.println(price);
+
+        int n = 4;
+        System.out.println(n);
+
+    }
+}
