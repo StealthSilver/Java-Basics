f3fe2fcc4e09eeb1182108a1f4745fbbed0eb131
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 18:37:49 +0530

    init

---
+++ 02/forteenth.java
@@ -0,0 +1,13 @@
+// explicit conversion
+
+import java.util.*;
+
+public class forteenth {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        float a = 56.99999f;
+        int b = (int) a;
+        System.out.println(b);
+    }
+}
