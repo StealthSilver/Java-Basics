1b9dd8b33c4f6ae4be2135811c8191ee08095649
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 18:49:00 +0530

    init

---
+++ 02/seventeenth.java
@@ -0,0 +1,14 @@
+
+public class seventeenth {
+    public static void main(String args[]) {
+        int a = 10;
+        float b = (float) 20.32;
+        long c = 32;
+        double d = 32;
+
+        // type promotion to double
+        double ans = a + b + c + d;
+        System.out.println(ans);
+    }
+
+}
