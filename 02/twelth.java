1b891b1011a60e35d1ff7a4e54104dbd703c4e6d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 18:28:26 +0530

    init

---
+++ 02/twelth.java
@@ -0,0 +1,14 @@
+// area of a circle 
+
+import java.util.*;
+
+public class twelth {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        float rad = sc.nextFloat();
+        float area = (float) (3.14 * rad * rad);
+
+        System.out.println("Area of the circle is" + area);
+    }
+}
