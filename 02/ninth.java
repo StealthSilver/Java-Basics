549d10c72113204e50ece6b557d82686edbd2d96
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 18:15:57 +0530

    init

---
+++ 02/ninth.java
@@ -0,0 +1,15 @@
+// taking the input from the user
+
+// importing the package which contains the scanner class
+import java.util.*;
+
+public class ninth {
+    public static void main(String args[]) {
+        // creating the new object from the scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // to get the complete line as the input
+        String newInput = sc.nextLine();
+        System.out.println(newInput);
+    }
+}
