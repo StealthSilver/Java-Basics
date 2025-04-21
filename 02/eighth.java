227feebb829753c3b6c19ca1b12f8c9f6c1462dc
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 18:12:51 +0530

    init

--- 02/eighth.java
+++ 02/eighth.java
@@ -7,5 +7,7 @@ public class eighth {
     public static void main(String args[]) {
         // creating the new object from the scanner class
         Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(input);
     }
 }
