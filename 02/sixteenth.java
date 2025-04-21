ec431988c70c43a32c34db49e28d8dc999de43af
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 18:45:29 +0530

    init

--- 02/sixteenth.java
+++ 02/sixteenth.java
@@ -3,7 +3,14 @@ public class sixteenth {
     public static void main(String args[]) {
         char a = 'a';
         char b = 'b';
+
+        // type promotion will be done automatically due to the opearation
         System.out.println(b - a);
 
+        short x = 5;
+        byte y = 43;
+        char c = 'c';
+        byte bt = (byte) (a + b + c);
+        System.out.println(bt);
     }
 }
