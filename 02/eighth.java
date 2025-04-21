435018e1518a0ee9d013d40037069b75a57adf2d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 18:11:31 +0530

    init

---
+++ 02/eighth.java
@@ -0,0 +1,9 @@
+// taking the input from the user
+
+import java.util.*;
+
+public class eighth {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
