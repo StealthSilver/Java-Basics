7f37baa8b957f04edff960e6bfb53ac3d64d81ef
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-21 19:02:21 +0530

    init

---
+++ 02/exercises/first.java
@@ -0,0 +1,16 @@
+// average of three numbers 
+
+import java.util.*;
+
+public class first {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        float average = ((a + b + c) / 3);
+        System.out.println(average);
+    }
+}
---
+++ 02/exercises/second.java
@@ -0,0 +1,14 @@
+// area of a square
+
+import java.util.*;
+
+public class second {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int side = sc.nextInt();
+        int area = side * side;
+
+        System.out.println(area);
+    }
+}
