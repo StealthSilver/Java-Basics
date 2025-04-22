c421e228bea95a2cbcfff482fe157f85e47a60f8
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 11:26:14 +0530

    init

---
+++ 04/eighth.java
@@ -0,0 +1,12 @@
+// ternary operator
+
+public class eighth {
+    public static void main(String args[]) {
+
+        int larger = (5 > 3) ? 5 : 3;
+        System.out.println(larger);
+
+        String type = (6 % 2 == 0) ? "even" : "odd";
+        System.out.println(type);
+    }
+}
