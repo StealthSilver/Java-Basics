ab5aa392db0241f8006659847e1a9eb20045e6fc
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 10:36:08 +0530

    init

--- 03/forth.java
+++ 03/forth.java
@@ -1,16 +1,20 @@
 // logical operator
 public class forth {
     public static void main(String args[]) {
+
+        // logical and
         System.out.println(true && true);
         System.out.println(true && false);
         System.out.println(false && true);
         System.out.println(false && false);
 
+        // logical or
         System.out.println(true || true);
         System.out.println(true || false);
         System.out.println(false || true);
         System.out.println(false || false);
 
+        // logical not
         System.out.println(!true);
         System.out.println(!false);
     }
