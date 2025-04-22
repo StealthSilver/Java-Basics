1566f4411773ca408b95a6dd04f22351e214563e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 10:39:39 +0530

    init

---
+++ 03/fifth.java
@@ -0,0 +1,21 @@
+// assignment operators
+
+public class fifth {
+    public static void main(String args[]) {
+        int a = 4;
+        int b = 8;
+        int c = 2;
+        int d = 5;
+
+        a += 3;
+        b -= 3;
+        c *= 3;
+        d /= 3;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
