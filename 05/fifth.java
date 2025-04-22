cf9e480f42a4b69270c523ed831bf2f5783d3928
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 19:08:10 +0530

    init

---
+++ 05/fifth.java
@@ -0,0 +1,16 @@
+// sum of first n natural numbers 
+
+public class fifth {
+    public static void main(String args[]) {
+        int a = 100;
+
+        int sum = 0;
+
+        while (a > 0) {
+            sum += a;
+            a--;
+        }
+
+        System.out.println("Sum is : " + sum);
+    }
+}
---
+++ 05/sixth.java
@@ -0,0 +1,5 @@
+// for loop 
+
+public class sixth {
+
+}
