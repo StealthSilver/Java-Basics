22f58cda2e7c16d7993aa0c3064bdb0f812e4d6c
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 18:57:04 +0530

    init

--- 05/first.java
+++ 05/first.java
@@ -4,7 +4,7 @@ public class first {
     public static void main(String args[]) {
 
         int counter = 3;
-        while (counter >= 0) {
+        while (counter > 0) {
             System.out.println("Hello");
             counter--;
         }
