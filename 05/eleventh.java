c47200bae9b2803aae81424383a0d65f8f206cb1
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 19:52:59 +0530

    init

---
+++ 05/eleventh.java
@@ -0,0 +1,13 @@
+// break statement 
+
+public class eleventh {
+    public static void main(String args[]) {
+        for (int i = 1; i < 5; i++) {
+            if (i == 2) {
+                break;
+            }
+            System.out.println(i);
+        }
+        System.out.println("out of the loop");
+    }
+}
