f10adfffad5e749d407f7a4e4fed6d4b3de1069d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 11:09:31 +0530

    init

--- 04/first.java
+++ 04/first.java
@@ -1,7 +1,7 @@
 // if-else statement
 public class first {
     public static void main(String args[]) {
-        int age = 76;
+        int age = 16;
 
         if (age >= 18) {
             System.out.println("you can drive");
---
+++ 04/third.java
@@ -0,0 +1,17 @@
+// multiple ifs 
+
+public class third {
+    public static void main(String args[]) {
+        int a = 4;
+
+        if (a >= 1) {
+            System.out.println("Greater than one");
+        }
+        if (a >= 2) {
+            System.out.println("Greater then two");
+        }
+        if (a >= 3) {
+            System.out.println("Greater then three");
+        }
+    }
+}
