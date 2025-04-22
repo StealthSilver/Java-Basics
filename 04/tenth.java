bb153e23c3d635f58bddd4208aba15f41ab388c2
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 11:32:22 +0530

    init

--- 04/tenth.java
+++ 04/tenth.java
@@ -8,10 +8,13 @@ public class tenth {
         switch (number) {
             case 1:
                 System.out.println("coffee");
+                break; // this stops the code after case 1
             case 2:
                 System.out.println("tea");
+                break;
             case 3:
                 System.out.println("milk");
+                break;
             default:
                 System.out.println("nothing");
         }
