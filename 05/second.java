5de2bd62427749dc77e302d25672dfa3b94d7ac0
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 18:58:23 +0530

    init

---
+++ 05/second.java
@@ -0,0 +1,9 @@
+// infinite loop
+public class second {
+    public static void main(String args[]) {
+        while (true) {
+            System.out.println("hello");
+        }
+        System.out.println("Infinite loop ended");
+    }
+}
