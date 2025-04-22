ba0028e208d1955e7f8f401732637acb9a16c3dc
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 19:11:11 +0530

    init

---
+++ 05/seventh.java
@@ -0,0 +1,9 @@
+// square pattern 
+
+public class seventh {
+    public static void main(String args[]) {
+        for (int i = 0; i < 4; i++) {
+            System.out.println("****");
+        }
+    }
+}
