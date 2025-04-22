e7b9cf0baf8c8032cad808c4d0a1b9732fe1c825
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 10:27:33 +0530

    init

---
+++ 03/second.java
@@ -0,0 +1,22 @@
+// unary operators
+
+public class second {
+    public static void main(String args[]) {
+        int a = 9;
+        int b = 5;
+
+        System.out.println(a++);
+        System.out.println(b--);
+
+        int c = 6;
+        int d = 4;
+
+        // pre-increment
+        ++c;
+        System.out.println(c);
+
+        // post-increment
+        d++;
+        System.out.println(d);
+    }
+}
