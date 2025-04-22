5d527b21efb4173e6859382f0b72f487422b028e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 11:31:07 +0530

    init

---
+++ 04/tenth.java
@@ -0,0 +1,19 @@
+// switch case statement 
+
+public class tenth {
+    public static void main(String args[]) {
+
+        int number = 2;
+
+        switch (number) {
+            case 1:
+                System.out.println("coffee");
+            case 2:
+                System.out.println("tea");
+            case 3:
+                System.out.println("milk");
+            default:
+                System.out.println("nothing");
+        }
+    }
+}
