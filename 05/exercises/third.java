5b2af5a85e866503f5990e57bfecc549df70db03
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 22:59:03 +0530

    final list

---
+++ 05/exercises/third.java
@@ -0,0 +1,19 @@
+// multiplication table 
+
+import java.util.*;
+
+public class third {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " * " + i + " = " + n * i);
+
+        }
+
+    }
+}
