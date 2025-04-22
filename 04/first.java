23bf58ac20e826b6fcaffecc25778c3f98e12d30
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 11:04:49 +0530

    init

---
+++ 04/first.java
@@ -0,0 +1,12 @@
+// if-else statement
+public class first {
+    public static void main(String args[]) {
+        int age = 76;
+
+        if (age >= 18) {
+            System.out.println("you can drive");
+        } else {
+            System.out.println("you cannot drive");
+        }
+    }
+}
