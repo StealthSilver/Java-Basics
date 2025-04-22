4370ec8347fbcbdd18bfec4de58a2ea44077d5b5
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 20:03:09 +0530

    init

---
+++ 05/thirteenth.java
@@ -0,0 +1,12 @@
+// continue statement 
+
+public class thirteenth {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
