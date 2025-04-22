e496f75f0814391ca1f216cbcc1c23a3ea458f1b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-23 01:28:08 +0530

    final list

---
+++ 06/third.java
@@ -0,0 +1,12 @@
+// half pyramid pattern 
+
+public class third {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
