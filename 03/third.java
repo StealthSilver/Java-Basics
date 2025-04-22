bfc40af67cabc503fb10133a5062deb8264df1a5
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 10:31:55 +0530

    init

---
+++ 03/third.java
@@ -0,0 +1,17 @@
+// relational operators 
+
+public class third {
+    public static void main(String args[]) {
+        int a = 4;
+        int b = 6;
+        int c = 9;
+        int d = 9;
+
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(c > d);
+        System.out.println(c < d);
+    }
+}
