8aa1c3461f5d01434989a9f2118a40c8ed5db47f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 19:17:38 +0530

    init

--- 05/eighth.java
+++ 05/eighth.java
@@ -1,7 +1,19 @@
 // reverse of a number 
 
+import java.util.*;
+
 public class eighth {
     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
 
+        int num = sc.nextInt();
+
+        String reverse = "";
+
+        while (num > 0) {
+            reverse += num % 10;
+            num = num % 10;
+        }
+        System.out.println(reverse);
     }
 }
