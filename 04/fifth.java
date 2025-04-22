851512ec4f6a047cef509f8fd8a28bedef284b95
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 11:14:45 +0530

    init

---
+++ 04/fifth.java
@@ -0,0 +1,16 @@
+// else if statements 
+
+public class fifth {
+    public static void main(String args[]) {
+
+        int age = 32;
+
+        if (age >= 18) {
+            System.out.println("Adult");
+        } else if (age >= 13 && age < 18) {
+            System.out.println("teen");
+        } else {
+            System.out.println("child");
+        }
+    }
+}
