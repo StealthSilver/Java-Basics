809e6edbc01bc312df915c08863f2a275bc50faa
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 19:09:46 +0530

    init

--- 05/sixth.java
+++ 05/sixth.java
@@ -1,5 +1,11 @@
 // for loop 
 
 public class sixth {
+    public static void main(String args[]) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Hello world");
+        }
+    }
 
 }
