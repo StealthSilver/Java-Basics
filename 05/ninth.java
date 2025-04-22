f88e3244cc93d10867dae95811046ad4e56fa00c
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-22 19:46:38 +0530

    init

---
+++ 05/ninth.java
@@ -0,0 +1,21 @@
+// reversing the given number 
+
+import java.util.*;
+
+public class ninth {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+
+        int num = sc.nextInt();
+        int rev = 0;
+
+        while (num > 0) {
+            int lastDigit = num % 10;
+            rev = (rev * 10) + lastDigit;
+            num = num / 10;
+        }
+
+        System.out.println(rev);
+    }
+}
