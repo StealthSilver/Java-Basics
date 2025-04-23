ba6c7ddb199d8559973b31eb570b1964ca30850b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-23 11:54:53 +0530

    renamed the given files

--- 07/first.java
+++ 07/first.java
@@ -2,7 +2,7 @@
 
 public class first {
 
-    // creating a new function
+    // creating a new function -> return type of hte function is void
     public static void printHello() {
         System.out.println("Hello");
     }
