3c8dd701d0ae5177580678b3dadd7251a35beda0
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-23 19:58:18 +0530

    renamed the given files

---
+++ 07/tenth.java
@@ -0,0 +1,20 @@
+// function to check the prime number
+
+import java.util.*;
+
+public class tenth {
+
+    public static boolean prime(int a) {
+
+        return true;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = sc.nextInt();
+
+        boolean result = prime(a);
+        System.out.println(result);
+    }
+}
