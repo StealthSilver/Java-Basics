fa8464e7a7a5751453b70064e4c450453d400a28
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-23 20:04:55 +0530

    renamed the given files

--- 07/eleventh.java
+++ 07/eleventh.java
@@ -1,5 +1,30 @@
 // prime numebr checker - optimised approach 
 
+import java.util.*;
+
 public class eleventh {
 
+    public static boolean prime(int n) {
+
+        if (n == 2) {
+            return true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+
+        boolean result = prime(n);
+        System.out.println(result);
+    }
 }
