34cb070482266ec4d26cf0d9614d0bb429797a00
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-23 19:51:25 +0530

    renamed the given files

--- 07/eighth.java
+++ 07/eighth.java
@@ -2,4 +2,18 @@
 
 public class eighth {
 
+    // first function
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    // second function
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+
+    }
+
+    public static void main(String args[]) {
+
+    }
 }
