798dd0ef0752978359e4c4d45c1c435c7f8751d1
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-23 11:51:37 +0530

    renamed the given files

---
+++ 07/first.java
@@ -0,0 +1,14 @@
+// functions 
+
+public class first {
+
+    // creating a new function
+    public static void printHello() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String args[]) {
+        // calling the function
+        printHello();
+    }
+}
