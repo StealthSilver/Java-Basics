8547b14ec023fbf14cf66d50997c0bdd126ec25f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-23 12:06:59 +0530

    renamed the given files

---
+++ 07/third.java
@@ -0,0 +1,19 @@
+// call by value in java
+
+public class third {
+    public static void main(String args[]) {
+
+        int a = 4;
+        int b = 9;
+
+        // swapping
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+}
