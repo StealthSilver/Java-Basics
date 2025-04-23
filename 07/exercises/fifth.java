7dd43e2f5c5bf1a97979ebea4bc6c21e9aa2fdf0
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-23 23:18:26 +0530

    first one out

--- 07/exercises/fifth.java
+++ 07/exercises/fifth.java
@@ -1,4 +1,23 @@
 // calculating the sum of digits of a number
+
 public class fifth {
 
+    public static int Sum(int a) {
+        int digitSum = 0;
+        ;
+
+        while (a > 0) {
+            int lastDigit = a % 10;
+            digitSum += lastDigit;
+            a /= 10;
+
+        }
+        return digitSum;
+    }
+
+    public static void main(String args[]) {
+        int a = 478374;
+        int result = Sum(a);
+        System.out.println(result);
+    }
 }
