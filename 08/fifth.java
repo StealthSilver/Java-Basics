ffae296685775baca0ceacbc3d6c120b40963402
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-24 11:02:57 +0530

    first one out

--- 08/fifth.java
+++ 08/fifth.java
@@ -2,4 +2,26 @@
 
 public class fifth {
 
+    public static void zero_one_triangle(int n) {
+
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+
+            // inner loop
+            for (int j = 1; j <= i; j++) {
+
+                // checking for even cells
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        zero_one_triangle(7);
+    }
 }
