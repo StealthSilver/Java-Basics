f637c9870b99623bc7addc1110f0f9ce0c4d7fb2
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-24 10:49:11 +0530

    first one out

--- 08/second.java
+++ 08/second.java
@@ -2,11 +2,24 @@
 
 public class second {
 
-    public static void inv_rot_pyramid() {
+    public static void inv_rot_pyramid(int n) {
+        // outer loop
+        for (int i = 1; i <= n; i++) {
 
+            // spaces
+            for (int j = 1; j <= n - 1; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+        }
+        System.out.println();
     }
 
     public static void main(String args[]) {
-
+        inv_rot_pyramid(5);
     }
 }
