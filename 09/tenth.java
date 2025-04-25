313118fe92ea4bd8578176500e1021c96ff3f4e8
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-25 15:01:00 +0530

    open

---
+++ 09/tenth.java
@@ -0,0 +1,30 @@
+// reversing an array without a new array
+// time O(n) and space O(1)
+
+public class tenth {
+
+    public static void reverse(int numbers[]) {
+        int first = 0, last = numbers.length - 1;
+
+        while (first < last) {
+
+            // swap
+            int temp = numbers[last];
+            numbers[last] = numbers[first];
+            numbers[first] = temp;
+
+            first++;
+            last--;
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] numbers = { 1, 2, 4, 32, 11, 3 };
+
+        reverse(numbers);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i] + " ");
+        }
+        System.out.println();
+    }
+}
