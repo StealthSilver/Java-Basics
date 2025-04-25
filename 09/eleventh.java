4a362dabd49d5409a04c5f2d18af2a338d3b6833
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-25 15:06:18 +0530

    open

--- 09/eleventh.java
+++ 09/eleventh.java
@@ -3,14 +3,19 @@
 public class eleventh {
 
     public static void pairs(int numbers[]) {
+
+        int totalPairs = 0
+
         for (int i = 0; i < numbers.length; i++) {
             int curr = numbers[i];
 
             for (int j = i + 1; j < numbers.length; j++) {
                 System.out.print("(" + curr + "," + numbers[j] + ") ");
+                totalPairs ++;
             }
             System.out.println();
         }
+        System.out.println("total pairs = " + totalPairs);
     }
 
     public static void main(String args[]) {
