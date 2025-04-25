c01f975894111cdd95fab64b7b23a3d5264920e9
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-25 18:26:59 +0530

    open

--- 10/third.java
+++ 10/third.java
@@ -3,8 +3,20 @@
 
 public class third {
 
-    public static int kadane(int[] number) {
+    public static int kadane(int[] numbers) {
 
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            currSum = currSum + numbers[i];
+            if (currSum < 0) {
+                currSum = 0;
+            }
+            maxSum = Math.max(currSum, maxSum);
+        }
+
+        return maxSum;
     }
 
     public static void main(String args[]) {
