7cbff080530bb0b3b30d248d3a7c1dedf6c53954
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-25 18:59:10 +0530

    open

--- 10/fifth.java
+++ 10/fifth.java
@@ -5,8 +5,30 @@ You want to maximize your profit by choosing a single day to buy one stock and c
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0. */
 
 public class fifth {
+
+    public static int stock(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                // profit
+                int profit = prices[i] - buyPrice; // today's profit
+
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i]; // updating the buying price
+            }
+        }
+
+        return maxProfit;
+    }
+
     public static void main(String args[]) {
 
         int prices[] = { 7, 1, 5, 3, 6, 4 };
+
+        int result = stock(prices);
+        System.out.println("the max profit is : " + result);
     }
 }
