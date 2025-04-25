1e5ed29f221ea767937917fbe8658c6da45550cd
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-25 15:15:56 +0530

    open

--- 09/twelth.java
+++ 09/twelth.java
@@ -5,11 +5,33 @@
 public class twelth {
 
     public static void subarray(int numbers[]) {
+        int ts = 0;
 
+        // outer loop -> start index
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+
+            // middle loop -> end index
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+
+                // inner loop -> print subarray from start to end
+                for (int k = start; k <= end; k++) {
+                    System.out.print(numbers[k] + " ");
+                }
+
+                ts++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+        System.out.println("total subarrays = " + ts);
     }
 
     public static void main(String args[]) {
         int numbers[] = { 2, 4, 6, 8, 10 };
 
+        subarray(numbers);
     }
 }
