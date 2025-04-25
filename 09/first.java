2ac94a4723a7dd8edbb79d5822a512ae9eae6d89
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-25 13:52:33 +0530

    open

--- 09/first.java
+++ 09/first.java
@@ -2,6 +2,14 @@
 
 public class first {
     public static void main(String args[]) {
+
+        // array of size 50, if the values are not assigned then null values are stored
         int marks[] = new int[50];
+
+        // array of size 3 is defined automatically and the elements are stored
+        int nums[] = { 1, 2, 3 };
+
+        // Array of type string
+        String fruit[] = { "Apple", "GUava", "Orange" };
     }
 }
