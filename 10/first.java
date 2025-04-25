2ebaaa184a62c85be64a2459a8e98af042a9a627
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-25 18:09:47 +0530

    open

---
+++ 10/first.java
@@ -0,0 +1,38 @@
+// maximum subarray sum
+
+import java.util.*;
+
+public class first {
+
+    public static int subarray(int[] number) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < number.length; i++) {
+            int start = i;
+
+            for (int j = i; j < number.length; j++) {
+                int end = j;
+                currSum = 0;
+
+                for (int k = start; k <= end; k++) {
+                    currSum += number[k];
+                }
+
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+
+            }
+        }
+        return currSum;
+
+    }
+
+    public static void main(String args[]) {
+        int[] number = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        int result = subarray(number);
+        System.out.println("the maximum subaraay sum is  " + result);
+    }
+}
