1ef151bb0ea9ef8cf84c3020a49593526a84d2f9
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-25 13:55:32 +0530

    open

---
+++ 09/second.java
@@ -0,0 +1,20 @@
+// input, putput and update in array
+
+import java.util.*;
+
+public class second {
+    public static void main(String args[]) {
+        int marks[] = new int[100];
+
+        Scanner sc = new Scanner(System.in);
+
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println(marks);
+        System.out.println("Phy marks: " + marks[0]);
+        System.out.println("Chem marks: " + marks[1]);
+        System.out.println("Mat marks: " + marks[2]);
+    }
+}
