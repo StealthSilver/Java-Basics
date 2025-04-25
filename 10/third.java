358efb5fbc4e93291d81c0f171caf87c07a46620
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-25 18:23:07 +0530

    open

---
+++ 10/third.java
@@ -0,0 +1,16 @@
+// prefix sum -> Kadane's Algorithm
+// Time complexity
+
+public class third {
+
+    public static int kadane(int[] number) {
+
+    }
+
+    public static void main(String args[]) {
+        int[] numbers = { 1, 2, -5, -6, 4, -3, 2, 6, 1 };
+
+        int result = kadane(numbers);
+        System.out.println("the maximum sum is : " + result);
+    }
+}
