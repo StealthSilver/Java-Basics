ce0444648a2c37c77c390e03bbedda3415d6715d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-25 15:07:17 +0530

    open

--- 09/eleventh.java
+++ 09/eleventh.java
@@ -1,17 +1,18 @@
 // pairs in an array
+// total pairs = n * (n-1) /  2 (sum of n-1 terms)
 
 public class eleventh {
 
     public static void pairs(int numbers[]) {
 
-        int totalPairs = 0
+        int totalPairs = 0;
 
         for (int i = 0; i < numbers.length; i++) {
             int curr = numbers[i];
 
             for (int j = i + 1; j < numbers.length; j++) {
                 System.out.print("(" + curr + "," + numbers[j] + ") ");
-                totalPairs ++;
+                totalPairs++;
             }
             System.out.println();
         }
