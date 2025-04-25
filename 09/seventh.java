722104151792ad7cf25de33873da9b49913d7a92
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-25 14:20:06 +0530

    open

---
+++ 09/seventh.java
@@ -0,0 +1,25 @@
+// smallest value in array 
+
+import java.util.*;
+
+public class seventh {
+
+    public static int mim(int numbers[]) {
+        int smallest = Integer.MAX_VALUE; // this signifies +infinity and it is a part of java.util
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (smallest > numbers[i]) {
+                smallest = numbers[i];
+            }
+        }
+        return smallest;
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = { 2, 3, 4, 1, 2, 3, 676, 42, 12, 9 };
+
+        int smallest = mim(numbers);
+
+        System.out.println("the smallest number in array is " + smallest);
+    }
+}
