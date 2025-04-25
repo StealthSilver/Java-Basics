37c48490afa956773df1669b9116839c8d3bdce4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-25 18:40:06 +0530

    open

--- 10/forth.java
+++ 10/forth.java
@@ -2,5 +2,7 @@
 // given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining
 
 public class forth {
+    public static void main(String args[]) {
 
+    }
 }
