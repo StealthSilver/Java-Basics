9bf4b4d2d718b84e64eea8778c42a531a8d7e7a2
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-25 15:17:22 +0530

    open

---
+++ 09/SubarraySums.java
@@ -0,0 +1,49 @@
+// max and min sum of all the subarrays
+
+public class SubarraySums {
+
+    public static void subarraySums(int numbers[]) {
+        int totalSubarrays = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int minSum = Integer.MAX_VALUE;
+
+        // Outer loop -> start index
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+
+            // Middle loop -> end index
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+                int sum = 0;
+
+                // Inner loop -> print subarray and calculate sum
+                System.out.print("Subarray: ");
+                for (int k = start; k <= end; k++) {
+                    System.out.print(numbers[k] + " ");
+                    sum += numbers[k];
+                }
+
+                // Update max and min sum
+                if (sum > maxSum) {
+                    maxSum = sum;
+                }
+                if (sum < minSum) {
+                    minSum = sum;
+                }
+
+                totalSubarrays++;
+                System.out.println("=> Sum = " + sum);
+            }
+        }
+
+        System.out.println("\nTotal subarrays = " + totalSubarrays);
+        System.out.println("Maximum subarray sum = " + maxSum);
+        System.out.println("Minimum subarray sum = " + minSum);
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = { 2, 4, 6, 8, 10 };
+
+        subarraySums(numbers);
+    }
+}
