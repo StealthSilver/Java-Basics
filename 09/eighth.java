1e5c740430749ff3f55d8b9545ad97009cddda6e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-25 14:28:21 +0530

    open

--- 09/eighth.java
+++ 09/eighth.java
@@ -2,9 +2,32 @@
 
 public class eighth {
 
-    public static int binarySearch(int number[] , int key)
+    public static int binarySearch(int numbers[], int key) {
+
+        int start = 0, end = numbers.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            // comparisions
+            if (numbers[mid] == key) {
+                return mid;
+            }
+            if (numbers[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1; // not found condition
+    }
 
     public static void main(String args[]) {
+        int numbers[] = { 2, 4, 5, 8, 10, 12, 14 };
+        int key = 10;
+
+        System.out.println("index for key is : " + binarySearch(numbers, key));
 
     }
 }
