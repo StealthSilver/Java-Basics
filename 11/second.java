dba4d842320fe398c69a9f88fff4fa3b22cd7d7b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-04-26 15:46:02 +0530

    open

---
+++ 11/second.java
@@ -0,0 +1,25 @@
+// Selection Sort 
+
+public class second {
+
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            // swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 5, 1, 3, 2, 4 };
+        selectionSort(arr);
+
+    }
+}
