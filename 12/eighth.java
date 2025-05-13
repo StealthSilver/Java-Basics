0b6479b66f53ad88a0ef341c3261614ad07cc7c0
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-14 02:13:34 +0530

    p

---
+++ 12/eighth.java
@@ -0,0 +1,23 @@
+public class eighth {
+
+    public static void main(String args[]) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.sum(1, 2, 3));
+        System.out.println(calc.sum(1.2f, 5.6f));
+    }
+
+}
+
+class Calculator {
+    int sum(int a, int b) {
+        return a + b;
+    }
+
+    float sum(float a, float b) {
+        return a + b;
+    }
+
+    int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+}
