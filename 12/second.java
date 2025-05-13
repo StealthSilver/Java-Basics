a4ad252e9d2761462281e4b2332ac9bcd1dade26
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-14 01:34:08 +0530

    p

--- 12/second.java
+++ 12/second.java
@@ -1,6 +1,9 @@
 public class second {
     public static void main(String args[]) {
+
         Student s1 = new Student("pogo");
+        Student s2 = new Student();
+        Student s3 = new Student(32);
     }
 }
 
@@ -9,7 +12,18 @@ class Student {
     int roll;
 
     // constructor with initialization
+
+    // non-parameterized constructos
+    Student() {
+        System.out.println("constructor is called ..");
+    }
+
+    // parameterized constructors
     Student(String name) {
         this.name = name;
+    }
+
+    Student(int roll) {
+        this.roll = roll;
     }
 }
