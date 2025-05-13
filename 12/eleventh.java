a043dccb6601e0ade89399173180c7ccf27a4546
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-14 02:42:06 +0530

    p

--- 12/eleventh.java
+++ 12/eleventh.java
@@ -1,5 +1,24 @@
-// interface 
 
 public class eleventh {
+    public static void main(String args[]) {
+        Queen q = new Queen();
+        q.moves();
+    }
+}
 
+interface ChessPlayer {
+    void moves();
+
+}
+
+class Queen implements ChessPlayer {
+    public void moves() {
+        System.out.println("up, down . left , right , diogmnal");
+    }
+}
+
+class Rook implements ChessPlayer {
+    public void moves() {
+        System.out.println("up, down . left , right ");
+    }
 }
