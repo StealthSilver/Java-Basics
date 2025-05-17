c6ffdd8db05ba4703ce2d7c3261af529a6a4568f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-17 08:15:30 +0530

    random also

---
+++ 15/twelth.java
@@ -0,0 +1,12 @@
+
+public class twelth {
+    public static void main(String args[]) {
+
+    }
+}
+
+class Student {
+    static int returnPercentage(int phy, int chem, int math) {
+        return (math + phy + chem) / 3;
+    }
+}
