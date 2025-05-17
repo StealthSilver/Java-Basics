f9222fe0ee434ea95f257e918ab81717a798530e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-17 08:28:54 +0530

    random also

--- 16 - Recursion/first.java
+++ 16 - Recursion/first.java
@@ -1,6 +1,19 @@
 // print numbers from n to 1 -> decreasing order
 public class first {
-    public static void main(String[] args) {
 
+    public static void printDec(int n) {
+        if (n == 1) {
+            System.out.println(n);
+            return;
+        }
+
+        System.out.println(n + " ");
+        ;
+        printDec(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        printDec(n);
     }
 }
