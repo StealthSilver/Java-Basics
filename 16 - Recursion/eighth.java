ce35c4829f48270fe2166ec6c498ad73f96c9258
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-18 19:01:45 +0530

    random also

---
+++ 16 - Recursion/eighth.java
@@ -0,0 +1,12 @@
+// find the last occourance of an element in an array
+public class eighth {
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 5, 6, 7 };
+        int a = 4;
+        System.out.println(lastOcc(arr, a));
+    }
+
+    public static int lastOcc(int[] arr, int a) {
+
+    }
+}
---
+++ 16 - Recursion/seventh.java
@@ -0,0 +1,22 @@
+// find the first occourance of an element in an array
+public class seventh {
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 5, 6, 7 };
+        int key = 4;
+        System.out.println(firstOcc(arr, key, 0));
+    }
+
+    public static int firstOcc(int[] arr, int key, int i) {
+
+        if (i == arr.length) {
+            return -1;
+        }
+
+        if (arr[i] == key) {
+            return i;
+        }
+
+        return firstOcc(arr, key, i+1)
+
+    }
+}
