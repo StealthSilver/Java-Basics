f5ee51f245097738661345e4c3fedb88a19b8683
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-18 18:51:20 +0530

    random also

--- 16 - Recursion/sixth.java
+++ 16 - Recursion/sixth.java
@@ -3,4 +3,12 @@ public class sixth {
     public static void main(String[] args) {
 
     }
+
+    public static boolean isSorted(int[] arr, int i) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+
+        return isSorted(arr, i + 1);
+    }
 }
