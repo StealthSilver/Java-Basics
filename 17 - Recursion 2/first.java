be91e60389996c1d5721230d04d838fec640b281
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-18 19:53:47 +0530

    random also

--- 17 - Recursion 2/first.java
+++ 17 - Recursion 2/first.java
@@ -1,5 +1,22 @@
 // tiling problem -> given a "2 * n" board and tiles of size "2*1", count the numebr id ways to tile the given board using the 2*1 tiles.
 // tiles can be places horizontally or vertically
-public class first {
 
+public class first {
+    public static void main(String args[]) {
+
+    }
+
+    public static int tilingProblem(int n) {
+
+        // base case
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        // vertical choice
+        int fnm1 = tilingProblem(n - 1);
+
+        // horizontal choice
+        int fnm2 = tilingProblem(n - 2);
+    }
 }
