1afa10db1f14e104d360313fc8cf42068af09783
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-18 19:10:07 +0530

    random also

---
+++ 16 - Recursion/ninth.java
@@ -0,0 +1,22 @@
+// print x to the power n
+public class ninth {
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 10;
+
+        System.out.println(power(x, n));
+    }
+
+    public static int power(int x, int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        // int xnm1 = power(x, n - 1);
+        // int xn = x * xnm1;
+        // return xn;
+
+        return x * power(x, n - 1);
+    }
+}
