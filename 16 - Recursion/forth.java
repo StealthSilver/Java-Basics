19e835380d815159ac1b2b341eac0f04dd15f387
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-18 11:09:53 +0530

    random also

--- 16 - Recursion/forth.java
+++ 16 - Recursion/forth.java
@@ -2,10 +2,14 @@
 public class forth {
     public static void main(String args[]) {
         int n = 10;
-        Sumn(n);
+        System.out.println(Sumn(n));
     }
 
     public static int Sumn(int n) {
+
+        if (n == 1) {
+            return 1;
+        }
         int Sumnm1 = Sumn(n - 1);
         int Sn = n + Sumnm1;
 
