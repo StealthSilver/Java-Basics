7a4395023b7a7f7c1d13dbed452f189faf98d0b8
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-18 19:02:55 +0530

    random also

--- 16 - Recursion/eighth.java
+++ 16 - Recursion/eighth.java
@@ -2,11 +2,11 @@
 public class eighth {
     public static void main(String[] args) {
         int[] arr = { 2, 3, 4, 5, 6, 7 };
-        int a = 4;
-        System.out.println(lastOcc(arr, a));
+        int key = 4;
+        System.out.println(lastOcc(arr, key, 0));
     }
 
-    public static int lastOcc(int[] arr, int a) {
+    public static int lastOcc(int[] arr, int key, int i) {
 
     }
 }
