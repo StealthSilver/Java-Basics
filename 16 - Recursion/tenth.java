138207bb12d1b4429de1bd49e218955aede77a8c
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-18 19:17:36 +0530

    random also

--- 16 - Recursion/tenth.java
+++ 16 - Recursion/tenth.java
@@ -3,6 +3,19 @@
 
 public class tenth {
     public static void main(String[] args) {
+        int x = 2;
+        int n = 10;
+        System.out.println(opPower(int x, int n));
+    }
 
+    public static int opPower(int a, int n) {
+        int halfPowerSq = opPower(a, n / 2) * opPower(a, n / 2);
+
+        // n is odd
+        if (n % 2 != 0) {
+            halfPowerSq = a * halfPowerSq;
+        }
+
+        return halfPowerSq;
     }
 }
