fb50c7c437589274b87acbd7b3a07da0b6566519
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-18 18:53:10 +0530

    random also

--- 16 - Recursion/sixth.java
+++ 16 - Recursion/sixth.java
@@ -1,10 +1,16 @@
 // check is the array is sorted or not
 public class sixth {
     public static void main(String[] args) {
-
+        int arr[] = { 1, 2, 3, 4, 5 };
+        System.out.println(isSorted(arr, 0));
     }
 
     public static boolean isSorted(int[] arr, int i) {
+
+        if (i == arr.length - 1) {
+            return true;
+        }
+
         if (arr[i] > arr[i + 1]) {
             return false;
         }
