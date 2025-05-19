83556c26c1958e0887fa7b781ebcfd432d9b9780
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-19 06:41:32 +0530

    random also

--- 18 - Divide & Conquer/first.java
+++ 18 - Divide & Conquer/first.java
@@ -4,12 +4,39 @@
 public class first {
 
     public static void mergeSort(int arr[], int si, int ei) {
-        // mid
 
+        // base case
+
+        if (si >= ei) {
+            return;
+        }
+
+        // mid
         int mid = si + (ei - si) / 2;
 
         mergeSort(arr, si, mid); // left
         mergeSort(arr, mid + 1, ei); // right
+
+        merge(arr, si, mid, ei);
+    }
+
+    public static void merge(int arr[], int si, int mid, int ei) {
+        int temp[] = new int[ei - si + 1];
+        int i = si; // for left part
+        int j = mid + 1; // for right part
+        int k = 0; // for temp arr
+
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+                k++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+                k++;
+            }
+        }
     }
 
     public static void printArr(int arr[]) {
