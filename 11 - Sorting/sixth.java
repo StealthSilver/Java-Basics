ef503709bbf4c43e7a1e4b8f75898c06e5f1e084
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-19 06:18:37 +0530

    random also

--- 11 - Sorting/sixth.java
+++ 11 - Sorting/sixth.java
@@ -3,7 +3,16 @@
 
 public class sixth {
     public static void main(String args[]) {
+        int arr[] = { 1, 4, 3, 2, 7, 6 };
+        countingSort(arr);
+        printArr(arr);
+    }
 
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 
     public static void countingSort(int arr[]) {
