9aa650ca08ce388c486d8aeaf24e95cd7f9cf4af
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-19 06:16:30 +0530

    random also

--- 11 - Sorting/sixth.java
+++ 11 - Sorting/sixth.java
@@ -1,7 +1,26 @@
 // Counting sort
+// time complexity O(n + range)
 
 public class sixth {
     public static void main(String args[]) {
 
+    }
+
+    public static void countingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        // sorting step
+        for (int i = 0; i < count.length; i++) {
+
+        }
     }
 }
