7c7485cab5a935dbe92793ca1e9a309b27039592
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-19 06:38:01 +0530

    random also

--- 18 - Divide & Conquer/first.java
+++ 18 - Divide & Conquer/first.java
@@ -3,4 +3,24 @@
 
 public class first {
 
+    public static void mergeSort(int arr[], int si, int ei) {
+        // mid
+
+        int mid = si + (ei - si) / 2;
+
+        mergeSort(arr, si, mid); // left
+        mergeSort(arr, mid + 1, ei); // right
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        mergeSort(arr, 0, arr.length - 1);
+    }
 }
