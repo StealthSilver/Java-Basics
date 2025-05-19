16e3b7dc3e7ac90619520933975800ffcc2c4dc1
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-19 07:05:10 +0530

    random also

---
+++ 18 - Divide & Conquer/third.java
@@ -0,0 +1,6 @@
+// search in a rotated sorted array
+// input : sorted rotated array with distinct numbers (in ascending order) is rotated at a pivot. find the index of given element
+
+public class third {
+
+}
