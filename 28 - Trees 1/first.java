62375e7174e476526610746b9d3f8a5b321c6a35
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-28 19:15:44 +0530

    trees

--- 28 - Trees 1/first.java
+++ 28 - Trees 1/first.java
@@ -1,7 +1,7 @@
 
 public class first {
 
-    public class Node {
+    static class Node {
         int data;
         Node left;
         Node right;
@@ -10,6 +10,24 @@ public class first {
             this.data = data;
             this.left = null;
             this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
         }
     }
 
