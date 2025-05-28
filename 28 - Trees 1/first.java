496598f0be7639c6dd9f1575e071203dbabb6c6d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-28 19:16:49 +0530

    trees

--- 28 - Trees 1/first.java
+++ 28 - Trees 1/first.java
@@ -33,6 +33,8 @@ public class first {
 
     public static void main(String args[]) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
-
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
     }
 }
